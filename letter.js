c0b1f072c97e9ccafb44e6abb6ad48c39422d68d
Ayushgupta208019 <116293093+Ayushgupta208019@users.noreply.github.com>
2024-03-22 12:19:23 +0530

    Add files via upload

---
+++ letter.js
@@ -0,0 +1,63 @@
+const letterS = document.getElementById('letter-1');
+const letterP = document.getElementById('letter-2');
+const letterO = document.getElementById('letter-3');
+const letterO2 = document.getElementById('letter-4');
+const letterR = document.getElementById('letter-5');
+const letterT = document.getElementById('letter-6');
+const letterH = document.getElementById('letter-7');
+const letterI = document.getElementById('letter-8');
+const messageContainer = document.querySelector('.message-container');
+const message = document.querySelector('.message');
+
+
+letterS.addEventListener('click', () => {
+  message.innerText = 'Spoorthi, you are soo beautifullll.';
+  messageContainer.style.display = 'flex';
+  messageContainer.style.animation = 'fadeIn 0.5s ease-in-out';
+})
+
+letterP.addEventListener('click', () => {
+  message.innerText = 'Spoorthi, you are soo Cuteeeeeee.....';
+  messageContainer.style.display = 'flex';
+  messageContainer.style.animation = 'fadeIn 0.5s ease-in-out';
+})
+
+letterO.addEventListener('click', () => {
+  message.innerText = 'Spoorthi, you are soo Charming......';
+  messageContainer.style.display = 'flex';
+  messageContainer.style.animation = 'fadeIn 0.5s ease-in-out';
+})
+
+letterO2.addEventListener('click', () => {
+  message.innerText = 'Spoorthi, you are soo Khoobsooratttt.';
+  messageContainer.style.display = 'flex';
+  messageContainer.style.animation = 'fadeIn 0.5s ease-in-out';
+})
+
+letterR.addEventListener('click', () => {
+  message.innerText = 'Spoorthi, you are very very lovelyyyyyyy....';
+  messageContainer.style.display = 'flex';
+  messageContainer.style.animation = 'fadeIn 0.5s ease-in-out';
+})
+
+letterT.addEventListener('click', () => {
+  message.innerText = 'Spoorthi, you are my very special friend...';
+  messageContainer.style.display = 'flex';
+  messageContainer.style.animation = 'fadeIn 0.5s ease-in-out';
+})
+
+letterH.addEventListener('click', () => {
+  message.innerText = 'Spoorthi, you are soo beautiful.\n Your Big Big Eyes and Small Mouth looking so cuteeeee......';
+  messageContainer.style.display = 'flex';
+  messageContainer.style.animation = 'fadeIn 0.5s ease-in-out';
+})
+
+letterI.addEventListener('click', () => {
+  message.innerText = 'Spoorthi, you are soo beautiful.\n Your long hair Hyee hyee inhance your beauty and make you attractive and gregarious.....';
+  messageContainer.style.display = 'flex';
+  messageContainer.style.animation = 'fadeIn 0.5s ease-in-out';
+})
+
+
+
+
